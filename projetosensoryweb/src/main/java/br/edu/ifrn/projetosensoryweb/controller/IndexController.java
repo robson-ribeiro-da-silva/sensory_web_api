8c400386350f489ba1209f1ac43e85deedc8f101
robson-ribeiro-da-silva <robson_r.silva@hotmail.com.br>
2019-09-30 16:09:05 -0300

    Commit - teste heroku

--- projetosensoryweb/src/main/java/br/edu/ifrn/projetosensoryweb/controller/IndexController.java
+++ projetosensoryweb/src/main/java/br/edu/ifrn/projetosensoryweb/controller/IndexController.java
@@ -7,20 +7,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "index";
-	}
-	
-	@RequestMapping(method=RequestMethod.POST,path= {"/index"})
-	public String login() {
-		return "index";
-	}
-	
-	@RequestMapping(method=RequestMethod.GET,path= {"/entrar"})
-	public String entrar() {
-		return "login";
-	}
-	
+//	@RequestMapping("/")
+//	public String index() {
+//		return "index";
+//	}
+//	
+//	@RequestMapping(method=RequestMethod.POST,path= {"/index"})
+//	public String login() {
+//		return "index";
+//	}
+//	
+//	@RequestMapping(method=RequestMethod.GET,path= {"/entrar"})
+//	public String entrar() {
+//		return "login";
+//	}
+//	
 	
 }
