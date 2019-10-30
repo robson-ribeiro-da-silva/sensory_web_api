0982603bdff7dbe3c0ee64c57a211964fe368f81
robson-ribeiro-da-silva <robson_r.silva@hotmail.com.br>
2019-10-30 15:09:38 -0200

    commit - teste

--- projetosensoryweb/src/main/java/br/edu/ifrn/projetosensoryweb/repository/AvaliacaoHedonicaRepository.java
+++ projetosensoryweb/src/main/java/br/edu/ifrn/projetosensoryweb/repository/AvaliacaoHedonicaRepository.java
@@ -11,7 +11,7 @@ import br.edu.ifrn.projetosensoryweb.model.AvaliacaoHedonica;
 @Repository
 public interface AvaliacaoHedonicaRepository extends JpaRepository<AvaliacaoHedonica, Long>{
 
-	@Query(value = "SELECT A.* FROM avaliacao_hedonica A INNER JOIN ESCALA_HAS_AVALIACAOHEDONICA EHA ON(A.ID = EHA.AVALIACAOHEDONICA_ID) INNER JOIN ESCALA E ON(EHA.ESCALA_ID = E.ID) INNER JOIN ANALISE_SENSORIAL AN ON(E.ID = AN.ESCALA_ID) WHERE AN.ID = ?1 AND  A.PERGUNTA = ?2", nativeQuery = true)
+	@Query(value = "select a.* from avaliacao_hedonica a inner join escala_has_avaliacaohedonica eha on(a.id = eha.avaliacaohedonica_id) inner join escala e on(eha.escala_id = e.id) inner join analise_sensorial an on(e.id = an.escala_id) where an.id = ?1 and  a.pergunta = ?2", nativeQuery = true)
 	public AvaliacaoHedonica findByIdAnaliseAndPergunta(Long id, String pergunta);
 	
 	@Query("select a from AvaliacaoHedonica a where a.id = ?1")
