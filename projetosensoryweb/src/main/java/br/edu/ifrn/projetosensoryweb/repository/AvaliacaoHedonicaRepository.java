c583d39cbd54451691aea7bfa5a7dbb6fd08ddd2
robson-ribeiro-da-silva <robson_r.silva@hotmail.com.br>
2019-10-30 15:02:18 -0200

    Commit - teste

--- projetosensoryweb/src/main/java/br/edu/ifrn/projetosensoryweb/repository/AvaliacaoHedonicaRepository.java
+++ projetosensoryweb/src/main/java/br/edu/ifrn/projetosensoryweb/repository/AvaliacaoHedonicaRepository.java
@@ -11,7 +11,7 @@ import br.edu.ifrn.projetosensoryweb.model.AvaliacaoHedonica;
 @Repository
 public interface AvaliacaoHedonicaRepository extends JpaRepository<AvaliacaoHedonica, Long>{
 
-	@Query(value = "SELECT A.* FROM AVALIACAO_HEDONICA A INNER JOIN ESCALA_HAS_AVALIACAOHEDONICA EHA ON(A.ID = EHA.AVALIACAOHEDONICA_ID) INNER JOIN ESCALA E ON(EHA.ESCALA_ID = E.ID) INNER JOIN ANALISE_SENSORIAL AN ON(E.ID = AN.ESCALA_ID) WHERE AN.ID = ?1 AND  A.PERGUNTA = ?2", nativeQuery = true)
+	@Query(value = "SELECT A.* FROM avaliacao_hedonica A INNER JOIN ESCALA_HAS_AVALIACAOHEDONICA EHA ON(A.ID = EHA.AVALIACAOHEDONICA_ID) INNER JOIN ESCALA E ON(EHA.ESCALA_ID = E.ID) INNER JOIN ANALISE_SENSORIAL AN ON(E.ID = AN.ESCALA_ID) WHERE AN.ID = ?1 AND  A.PERGUNTA = ?2", nativeQuery = true)
 	public AvaliacaoHedonica findByIdAnaliseAndPergunta(Long id, String pergunta);
 	
 	@Query("select a from AvaliacaoHedonica a where a.id = ?1")
